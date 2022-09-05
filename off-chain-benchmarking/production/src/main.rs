4cd6f84373a52176e961bd181671442c3ef34205
radiken <547806317@qq.com>
2022-09-05 21:44:08 +0100

    update off chain benchmarking functions

--- off-chain-benchmarking/production/src/main.rs
+++ off-chain-benchmarking/production/src/main.rs
@@ -9,15 +9,17 @@ use ed25519_dalek::Keypair;
 use ed25519_dalek::{Signature, Signer};
 use bls_signatures::*;
 use bls_signatures::Signature as bls_Signatures;
+use bls_signatures::PrivateKey as bls_PrivateKey;
 use std::time::{Duration, Instant};
 
 fn main() {
-    measure_multiple_different_messages();
+    measure_single_message_various_length();
 }
 
 pub fn measure_multiple_different_messages() {
     let mut time: Vec<(i32, u128, u128)> = Vec::new(); 
     let mut num_of_msg: i32 = 1;
+    let NUM_OF_ITERATION = 100;
     while num_of_msg <= 2048{
         
         let rng = &mut rand8::thread_rng();
@@ -28,12 +30,16 @@ pub fn measure_multiple_different_messages() {
         let keypairs: Vec<_> = (0..num_of_msg).map(|_| Keypair::generate(&mut csprng)).collect();
         let mut j: usize = 0;
         let eddsa_sigs = msgs.iter().zip(&keypairs).map(|(msg, keypair)| keypair.sign(msg)).collect::<Vec<Signature>>();
-        let eddsa_start = Instant::now();
-        while j < num_of_msg.try_into().unwrap(){
-            assert!(keypairs[j].verify(&msgs[j], &eddsa_sigs[j]).is_ok());
-            j += 1;
+        let mut eddsa_duration_sum = 0;
+        for  _ in 0..NUM_OF_ITERATION {
+            let eddsa_start = Instant::now();
+            while j < num_of_msg.try_into().unwrap(){
+                assert!(keypairs[j].verify(&msgs[j], &eddsa_sigs[j]).is_ok());
+                j += 1;
+            }
+            let eddsa_duration = eddsa_start.elapsed().as_micros();
+            eddsa_duration_sum += eddsa_duration;
         }
-        let eddsa_duration = eddsa_start.elapsed().as_micros();
 
         // bls 
         let private_keys: Vec<_> = (0..num_of_msg).map(|_| PrivateKey::generate(rng)).collect();
@@ -41,11 +47,15 @@ pub fn measure_multiple_different_messages() {
         let sigs = msgs.iter().zip(&private_keys).map(|(msg, pk)| pk.sign(msg)).collect::<Vec<bls_Signatures>>();
         let aggregated_sig = aggregate(&sigs[..]).unwrap();
         let hashes = msgs.iter().map(|msg| hash(msg)).collect::<Vec<_>>();
-        let bls_start = Instant::now();
-        verify(&aggregated_sig, &hashes, &public_keys);
-        let bls_duration = bls_start.elapsed().as_micros();
-
-        time.push((num_of_msg, eddsa_duration, bls_duration));
+        let mut bls_duration_sum = 0;
+        for _ in 0..NUM_OF_ITERATION {
+            let bls_start = Instant::now();
+            verify(&aggregated_sig, &hashes, &public_keys);
+            let bls_duration = bls_start.elapsed().as_micros();
+            bls_duration_sum += bls_duration;
+        }
+        
+        time.push((num_of_msg, eddsa_duration_sum/NUM_OF_ITERATION, bls_duration_sum/NUM_OF_ITERATION));
 
         num_of_msg *= 2;
     }
@@ -54,8 +64,8 @@ pub fn measure_multiple_different_messages() {
 }
 
 
-pub fn measure_single_message_verious_length() {
-    let num_of_iter = 10;
+pub fn measure_single_message_various_length() {
+    let num_of_iter = 100;
     let mut i = 1;
     let mut time: Vec<(i32, u128, u128)> = Vec::new(); 
     while i <= 100{
@@ -69,8 +79,8 @@ pub fn measure_single_message_verious_length() {
 
         let eddsa_start = Instant::now();
         let mut j = 0;
+        let signature: Signature = keypair.sign(&msg);
         while j < num_of_iter{
-            let signature: Signature = keypair.sign(&msg);
             assert!(keypair.verify(&msg, &signature).is_ok());
             j += 1;
         }
@@ -82,8 +92,8 @@ pub fn measure_single_message_verious_length() {
 
         let bls_start = Instant::now();
         let mut k = 0;
+        let sig = private_key.sign(&msg);
         while k < num_of_iter{
-            let sig = private_key.sign(&msg);
             let hash = vec![hash(&msg)];
             verify(&sig, &hash, &public_key);
             k += 1;
@@ -96,3 +106,4 @@ pub fn measure_single_message_verious_length() {
     }
     println!("{:?}", time);
 }
+
