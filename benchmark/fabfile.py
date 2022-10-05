dec44164d9a21690cd96dc845f63d48255ebc60e
radiken <547806317@qq.com>
2022-10-05 10:17:27 +0100

    remove transaction verifications

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -97,8 +97,8 @@ def remote(ctx):
     ''' Run benchmarks on AWS '''
     bench_params = {
         'faults': 0,
-        'nodes': [10, 20],
-        'rate': [10_000, 30_000],
+        'nodes': [4],
+        'rate': [50_000],
         'tx_size': 512,
         'duration': 300,
         'runs': 5,
